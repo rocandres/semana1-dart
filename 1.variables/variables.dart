1ef04995840347242bf017fe9aeb11cbecf3e0a1
Andres  Rodriguez <36413311+rcandres@users.noreply.github.com>
2021-09-04 08:17:43 -0500

    Ejercicio Variables

---
+++ 1.variables/variables.dart
@@ -0,0 +1,21 @@
+void main(){
+  var nombre = "Juan";
+  String nombre_dos ="Juan";
+
+  var edad=89;
+  int edad_dos=34;
+  var test;
+  test=100;
+
+  // null safety
+  int? variableNull= null;
+  String? variableStringNull= null;
+
+  //Final - constante
+  final String variableFinalTest ="hola";
+  const String variableconstanteTest="hola";
+
+  print(nombre_dos.toUpperCase());
+  print(nombre);
+  print(test);
+}
